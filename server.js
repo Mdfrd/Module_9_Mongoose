826a64052b5b1a64492e1d21139efdd7f6e669d7
Muhammad Farid <118275151+Mdfrd@users.noreply.github.com>
2023-03-31 03:48:20 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+let dbConnect = require("./dbConnect");
+// parse requests of content-type - application / json
+
+app.use(express.json());
+
+// app.get("/", (res) => {
+//     res.json({
+//         message: "Welcome to my MongoDB application."
+//     });
+// });
+
+let userRoutes = require('./routes/userRoutes')
+app.use("/api/users", userRoutes)
+
+// set port, listen for requests
+const PORT = process.env.PORT || 1080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
